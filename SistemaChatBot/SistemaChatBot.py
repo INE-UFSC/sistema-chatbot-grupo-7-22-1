95f892103497c5f66dd69601d61197cd0f036d31
Tiago Oliveira da Luz <tiagoluz64@gmail.com>
2022-05-13 10:55:03 -0300

    implemented SistemaChatBot

--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -4,23 +4,47 @@ class SistemaChatBot:
     def __init__(self,nomeEmpresa,lista_bots):
         self.__empresa=nomeEmpresa
         ##verificar se a lista de bots contém apenas bots
-        self.__lista_bots=lista_bots
+        if all(isinstance(x, Bot) for x in lista_bots):
+            self.__lista_bots=lista_bots
         self.__bot = None
     
+    @property
+    def empresa(self):
+        return self.__empresa
+    
+    @empresa.setter
+    def empresa(self, nomeEmpresa):
+        self.__empresa = nomeEmpresa
+
+    @property
+    def lista_bots(self):
+        return self.__lista_bots
+    
+    @property
+    def bot(self):
+        return self.__bot
+
+    @bot.setter
+    def bot(self, bot):
+        self.__bot = bot
+
     def boas_vindas(self):
-        pass
-        ##mostra mensagem de boas vindas do sistema
+        print("Bem vindo ao sistema da empresa", self.empresa)
 
     def mostra_menu(self):
-        pass
-        ##mostra o menu de escolha de bots
+        print("Os chatbots disponíveis são:")
+        for i, bot in enumerate(self.lista_bots()):
+            print(i, "- Bot:", bot.nome, ":", bot.boas_vindas)
+
     
     def escolhe_bot(self):
         pass
         ##faz a entrada de dados do usuário e atribui o objeto ao atributo __bot 
+        i = int(input("Digite com qual chat bot deseja conversar:"))
+        self.bot(self.lista_bots[i])
 
     def mostra_comandos_bot(self):
-        pass
+        self.bot.mostra_comandos()
         ##mostra os comandos disponíveis no bot escolhido
 
     def le_envia_comando(self):
