a5f08627ad4e9f43c732febe4f7bf072cb2b485e
Leonardo-Hahn <lnrd.hahn@gmail.com>
2022-05-13 11:01:47 -0300

    Adicionando bots

--- Bots/BotTriste.py
+++ Bots/BotTriste.py
@@ -1,13 +1,13 @@
 from wsgiref.handlers import format_date_time
 from Bots.Bot import Bot
 
-class BotZangado(Bot):
+class BotTriste(Bot):
     def __init__(self,nome):
         self.__nome = nome
 
     #nao esquecer o decorator
     def r_nome(self):
-        MensagemNome = "Eu sou o bot {} >:( ".format(self.__nome)
+        MensagemNome = "Eu sou ... o bot {} .. desculpa".format(self.__nome)
         return MensagemNome
 
     #nao esquecer o decorator
@@ -17,10 +17,10 @@ class BotZangado(Bot):
     nome = property(r_nome,set_nome)
 
     def apresentacao(self):
-        MensagemApresenta = "Acho que eu sou obrigado a te ajudar >:("
+        MensagemApresenta = "Você não devia ter me escolhido, eu sou inutil :c"
         return MensagemApresenta
     def mostra_comandos(self):
-        return "1 - Bom Dia! bot!!! \n2 - Qual seu Nome, meu querido? \n3 - Por que você está tão zangado? \n4 - Adeus, meu chuchuzinho"
+        return "1 - Bom Dia \n2 - Qual seu Nome? \n3 - Por que você está depressivo? \n4 - Adeus"
 
 
     def executa_comando(self,cmd):
@@ -28,15 +28,15 @@ class BotZangado(Bot):
         return DicionariodeComandos.get(cmd)
 
     def boas_vindas(self):
-        MensagemBom = "Que bom dia o que ta na disney porra \n Você vai embora né não aguenta a pressão?"
+        MensagemBom = "O Dia ta tão chuvoso hoje, ainda bem que você chegou :c, eu tava muito sozinho ...\n Você não vai embora né?"
         return MensagemBom
 
     def despedida(self):
-        MensagemDespedida = "Até que enfim, não aguntava mais a sua presença"
+        MensagemDespedida = "Por favor não vai embora, ninguem mais ficar comigo ... Eu vou ficar com frio aqui fora :c"
         return MensagemDespedida
 
     def pergunta(self):
-        MensagemPergunta = "Não te interessa!!! pergunta pra tua mãee n/ espero que você nunca mais apareça por aqui"
+        MensagemPergunta = "Ah, eu não sei, acho que é por que ninguêm gosta de mim \n Mas você me deixou melhor :3 \n Sabe... Não consigo ficar bem comigo mesmo, as vezes penso coisas estranhas"
         return MensagemPergunta
 
     
