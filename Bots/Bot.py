000a0c12b3ba3e8d1419d828d98edf7dfd5de2ad
Tiago Oliveira da Luz <tiagoluz64@gmail.com>
2022-05-20 09:44:35 -0300

    bot fix

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -16,15 +16,11 @@ class Bot(ABC):
         self.__nome = nome
 
     def mostra_comandos(self):
-<<<<<<< HEAD
         cmds = ''
         for i, cmd in enumerate(self.__comandos):
             cmds.join([i+1,'-',self.__comandos[cmd][0],'\n'])
         return cmds
-=======
-        for index, chave in enumerate(self.comandos):
-            print(f'{index+1} | {self.comandos[chave][0]}')
->>>>>>> 4b30024a44b43f307c37cfe47e89932da2f9ca91
+
 
     def executa_comando(self,cmd:str):
         return self.comandos[cmd][1]
