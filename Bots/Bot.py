f277f509fc19246c03fb59908fdca1a815a7be3b
Tiago Oliveira da Luz <tiagoluz64@gmail.com>
2022-05-20 09:37:03 -0300

    bot fix

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -5,24 +5,26 @@ import random as r
 
 class Bot(ABC):
 
-    def __init__(nome,):
-        self.nome = nome
-        self.comandos = {}
+    def __init__(self, nome):
+        self.__nome = nome
+        self.__comandos = {}
 
-    #nao esquecer o decorator
+    @property
     def nome(self):
-        pass
+        return self.__nome
 
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
 
     def mostra_comandos(self):
+        for i, cmd in enumerate(self.__comandos):
+            print(i+1,'-',self.__comandos[cmd][0])
         pass
 
     @abstractmethod
     def executa_comando(self,cmd):
-        pass
+        return self.__comandos[cmd][1]
 
     @abstractmethod
     def boas_vindas():
